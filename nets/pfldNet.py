d2e2d0268899522492c0585004bb2fbf7c4b5a9f
dylanin1999 <dylanin1999@163.com>
2022-08-25 23:40:34 +0800

    add conv2

--- nets/pfldNet.py
+++ nets/pfldNet.py
@@ -4,3 +4,13 @@ import torch
 from torchsummary import summary
 
 
+class bottleNect(nn.Sequential):
+    def __init__(self,intput,inputChannel,outChannel,kernelSize,strides):
+        paddings = kernelSize-1
+
+    def Conv2d(self):
+        x = nn.Sequential(
+            nn.Conv2d(in_channels=self.inputChannel, out_channels=self.outChannel, kernel_size=self.kernelSize,
+                      stride=self.strides, padding=self.paddings, bias=False)
+        )
+
