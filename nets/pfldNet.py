fee9db325a0f92f27c59f012d6c7b17c8c75c913
dylanin1999 <dylanin1999@163.com>
2022-08-26 23:11:32 +0800

    add Dwise Conv

--- nets/pfldNet.py
+++ nets/pfldNet.py
@@ -4,13 +4,36 @@ import torch
 from torchsummary import summary
 
 
-class bottleNect(nn.Sequential):
+class bottleNeck(nn.Sequential):
     def __init__(self,intput,inputChannel,outChannel,kernelSize,strides):
-        paddings = kernelSize-1
+        self.paddings = kernelSize-1
+        self.inputChannel = inputChannel
+        self.outChannel = outChannel
+        self.strides = strides
 
-    def Conv2d(self):
+    def Conv2dRelu6(self):
         x = nn.Sequential(
-            nn.Conv2d(in_channels=self.inputChannel, out_channels=self.outChannel, kernel_size=self.kernelSize,
-                      stride=self.strides, padding=self.paddings, bias=False)
+            nn.Conv2d(in_channels=self.inputChannel, out_channels=self.outChannel, kernel_size=1,
+                      stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(self.outChannel),
+            nn.ReLU6(True)
         )
+
+    def Conv2dLinear(self):
+        x = nn.Sequential(
+            nn.Conv2d(in_channels=self.inputChannel, out_channels=self.outChannel, kernel_size=1,
+                      stride=1, padding=0, bias=False),
+
+        )
+    def Dwise(self):
+        x = nn.Sequential(
+            nn.Conv2d(in_channels=self.outChannel,out_channels=self.outChannel,kernel_size=3,stride=self.strides,groups=self.outChannel),
+            nn.BatchNorm2d(self.outChannel),
+            nn.ReLU6(True)
+        )
+
+    def forward(self, input):
+        x = self.Conv2dRelu6(input)
+        x = self.Dwise(x)
+        x = self.Conv2dLinear(x)
 
