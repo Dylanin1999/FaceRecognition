6cad2adef085c7320a2f421f34390524b53f67f3
dylanin1999 <dylanin1999@163.com>
2022-08-29 23:14:23 +0800

    add mobileNetv2

--- nets/pfldNet.py
+++ nets/pfldNet.py
@@ -4,36 +4,66 @@ import torch
 from torchsummary import summary
 
 
-class bottleNeck(nn.Sequential):
-    def __init__(self,intput,inputChannel,outChannel,kernelSize,strides):
+class InvertedResidual(nn.Module):
+    def __init__(self,inputChannel,outChannel,kernelSize,strides,expand_ratio):
+        super(InvertedResidual, self).__init__()
         self.paddings = kernelSize-1
         self.inputChannel = inputChannel
         self.outChannel = outChannel
         self.strides = strides
+        self.hiddenlayerChannel = self.inputChannel*expand_ratio
+        self.Conv2dRelu6 = nn.Sequential(
+                nn.Conv2d(in_channels=self.inputChannel, out_channels=self.hiddenlayerChannel, kernel_size=1,
+                          stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(self.hiddenlayerChannel),
+                nn.ReLU6(True)
+            )
 
-    def Conv2dRelu6(self):
-        x = nn.Sequential(
-            nn.Conv2d(in_channels=self.inputChannel, out_channels=self.outChannel, kernel_size=1,
-                      stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(self.outChannel),
-            nn.ReLU6(True)
-        )
+        self.Conv2dLinear = nn.Sequential(
+                nn.Conv2d(in_channels=self.hiddenlayerChannel, out_channels=self.outChannel, kernel_size=1,
+                          stride=1, padding=0, bias=False),
 
-    def Conv2dLinear(self):
-        x = nn.Sequential(
-            nn.Conv2d(in_channels=self.inputChannel, out_channels=self.outChannel, kernel_size=1,
-                      stride=1, padding=0, bias=False),
-
-        )
-    def Dwise(self):
-        x = nn.Sequential(
-            nn.Conv2d(in_channels=self.outChannel,out_channels=self.outChannel,kernel_size=3,stride=self.strides,groups=self.outChannel),
-            nn.BatchNorm2d(self.outChannel),
-            nn.ReLU6(True)
-        )
+            )
+        self.Dwise = nn.Sequential(
+                nn.Conv2d(in_channels=self.hiddenlayerChannel,out_channels=self.hiddenlayerChannel,kernel_size=3,
+                          stride=self.strides,groups=self.hiddenlayerChannel,padding=1),
+                nn.BatchNorm2d(self.hiddenlayerChannel),
+                nn.ReLU6(True)
+            )
 
     def forward(self, input):
+        print(input.size())
         x = self.Conv2dRelu6(input)
+        print(x.size())
         x = self.Dwise(x)
-        x = self.Conv2dLinear(x)
+        print(x.size())
+        out = self.Conv2dLinear(x)
+        print(out.size())
+        if self.strides == 1:
+            print("!!!!RES ADD")
+            out =out+input
+        print(out.size())
+        return x
+
+
+class MobileNetV2(nn.Module):
+
+
+    def __init__(self):
+        super(MobileNetV2, self).__init__()
+
+        self.BottleneckCfg = [
+            [2, 64, 5, 2],
+            [2, 128, 1, 2],
+            [4, 128, 6, 1],
+            [2, 16, 1, 1]
+        ]
+    def forward(self):
+        for i in self.BottleneckCfg:
+            bottleneck = InvertedResidual(i[0],i[1],i[2],i[3])
+
+
+
+
+
 
