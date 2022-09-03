4900c332aefd44256208188fc7540994bd285a64
dylanin1999 <dylanin1999@163.com>
2022-09-03 23:09:53 +0800

    finished PFLD net

--- nets/pfldNet.py
+++ nets/pfldNet.py
@@ -53,23 +53,57 @@ class MobileNetV2(nn.Module):
             [2, 16, 1, 1]
         ]
 
-        #input kernel size,outchannel,stride
+        #input kernel size,outchannel,stride,padding``
         self.auxiliaryCfg = [
-            [3, 128, 2],
-            [3, 128, 1],
-            [3, 32, 2],
-            [7, 128, 1],
+            [3, 128, 2,1],
+            [3, 128, 1,1],
+            [3, 32, 2,1],
+            [7, 128, 1,0],
         ]
 
-        self.endConv = nn.Sequential(
-            nn.Conv2d(in_channels=self.BottleneckCfg[-1][1],out_channels=32,kernel_size=3,stride=2,padding=1),
-            nn.Conv2d(in_channels=32, out_channels=128, kernel_size=7, stride=1),
+
+
+    def endConv(self,input):
+        s1 = input
+        print("s1 Shape:", s1.size())
+        s2 = nn.Conv2d(in_channels=self.BottleneckCfg[-1][1],out_channels=32,kernel_size=3,stride=2,padding=1)(s1)
+        print("s2 Shape:", s2.size())
+        s3 = nn.Conv2d(in_channels=32, out_channels=128, kernel_size=7, stride=1)(s2)
+
+        s1 = nn.AvgPool2d(kernel_size=s1.size()[-1])(s1)
+        s2 = nn.AvgPool2d(kernel_size=s2.size()[-1])(s2)
+        # s1 = torch.flatten(s1, 1)
+        # s2 = torch.flatten(s2, 1)
+        # s3 = torch.flatten(s3, 1)
+        print("s1 Shape:", s1.size())
+        print("s2 Shape:", s2.size())
+        print("s3 Shape:", s3.size())
+        out = torch.cat([s1, s2, s3], 1)
+        out = torch.flatten(out,1)
       #      nn.Conv2d(in_channels=128,kernel_siz)
-        )
+        return out
 
-    def AuxiliaryNet(self):
+    #
+
+    def baseConv(self,input,kernelSize,outChannel,stride,padding):
+        out = nn.Conv2d(in_channels=input.size()[1],kernel_size=kernelSize,out_channels=outChannel,stride=stride,padding=padding)(input)
+        return out
+
+
+    def AuxiliaryNet(self,input):
+        x = input
         for layer, i in enumerate(self.auxiliaryCfg):
+            kernelSize = i[0]
+            stride = i[2]
+            padding = i[3]
 
+            x = self.baseConv(input=x,kernelSize=kernelSize,outChannel=i[1],stride=stride,padding=padding)
+            print("aux size: ",x.size())
+        x = torch.flatten(x,1)
+        x = nn.Linear(x.size()[1], 32)(x)
+        x = nn.Linear(x.size()[1], 3)(x)
+        print("aux out size: ", x.size())
+        return x
 
     def forward(self,input):
         x = nn.Conv2d(in_channels=3,out_channels=self.BottleneckCfg[0][1],kernel_size=3,stride=2,padding=1)(input)
@@ -84,21 +118,24 @@ class MobileNetV2(nn.Module):
                 else:
                     bottleneck = InvertedResidual(i[1], i[1], 1,i[0])
                 x = bottleneck(x)
-                if layer==0:
-                    Auxout = x
+            if layer==0:
+                Auxint = x
+                print("AUX input size: ",Auxint.size())
+                auxout = self.AuxiliaryNet(Auxint)
 
-        x = self.endConv(x)
 
+        print("out Shape:", x.size())
+        out = self.endConv(x)
 
-
-        print("out Shape:",x.size())
-        return x
+        out = nn.Linear(out.size()[1],136)(out)
+        return out,auxout
 
 x = torch.rand(4,3,224,224)
 
 net = MobileNetV2()
 
-out,Aux = net(x)
+out,auxout = net(x)
+print("out shape: ",out.size())
 
 
 
